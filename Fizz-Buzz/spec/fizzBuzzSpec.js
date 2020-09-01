64037422bf2f9d2d3eaa7e4adab72da53a860645
Craig Ryan <craig@craigryan.org>
2020-09-01 14:49:08 +0000

    Initial commit. Coding challenge using JavaScript & Jasmine test driven develpment.

---
+++ Fizz-Buzz/spec/fizzBuzzSpec.js
@@ -0,0 +1,33 @@
+describe("My fizzBuzz function", function() {
+    beforeEach(function() {
+        fizzBuzz = new fizzBuzz();
+});
+
+    describe("Returns number, fizz, buzz or fizzbuzz", function(){
+        it("Should exist", function(){
+            expect(fizzBuzz).toBeDefined();
+        });
+
+        it("Should return Fizz when called as fizzBuzz(9)", function(){
+            var result = fizzBuzz(9)
+            expect(result).toBe("Fizz");
+        });
+
+        it("Should return Buzz when called as fizzBuzz(10)", function(){
+            var result = fizzBuzz(10)
+            expect(result).toBe("Buzz");
+        });
+
+        it("Should return FizzBuzz when called as fizzBuzz(15)", function(){
+            var result = fizzBuzz(15)
+            expect(result).toBe("FizzBuzz");
+        });
+
+        it("Should return 2 when called as fizzBuzz(2)", function(){
+            var result = fizzBuzz(2)
+            expect(result).toBe(2);
+        });
+
+    });
+
+});
